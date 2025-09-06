8fe85c11582469f0670b8a8c2676e777851e9b3f
Daniel Bast <2790401+dbast@users.noreply.github.com>
2025-09-06 09:55:48 +0200

    Add pypi_to_conda mapping tool (#27)

--- conda_meta_mcp/tools/__init__.py
+++ conda_meta_mcp/tools/__init__.py
@@ -3,6 +3,7 @@ from .import_mapping import register_import_mapping
 from .info import register_info
 from .pkg_insights import register_package_insights
 from .pkg_search import register_package_search
+from .pypi_to_conda import register_pypi_to_conda
 
 TOOLS = [
     register_cli_help,
@@ -10,6 +11,7 @@ TOOLS = [
     register_package_insights,
     register_package_search,
     register_import_mapping,
+    register_pypi_to_conda,
 ]
 
 __all__ = ["TOOLS"]
